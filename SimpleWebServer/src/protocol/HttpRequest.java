068ba88423f1e76af55def9caf7c5e63d64773cd
Michael Eaton <eatonmi@rose-hulman.edu>
2012-10-28 23:37:08 -0400

    Don't yell at me commit

--- SimpleWebServer/src/protocol/HttpRequest.java
+++ SimpleWebServer/src/protocol/HttpRequest.java
@@ -25,7 +25,10 @@ package protocol;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -69,6 +72,24 @@ public class HttpRequest {
 	 */
 	public String getVersion() {
 		return version;
+	}
+	
+	public long getIfModified()
+	{
+		String value = header.get("If-Modified-Since");
+		if(value != null)
+		{
+			SimpleDateFormat format = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy");
+			try {
+				return format.parse(value).getTime();
+			} catch (ParseException e) {
+				return new Date().getTime();
+			}
+		}
+		else
+		{
+			return new Date().getTime();
+		}
 	}
 
 	/**
